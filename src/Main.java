9641f62bcd8e162b0d217de7def7ff2ebbf0c99d
alexandresanchograca@gmail.com <alexandresanchograca@gmail.com>
2023-10-14 19:05:53 +0100

    Removed files

--- src/Main.java
+++
@@ -1,7 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-
-        Field field = new Field();
-        Character character = new Character(field.getHeight(), field.getWidth());
-    }
-}
