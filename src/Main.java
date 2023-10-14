b1a1f3a00ba8fd2d343b3a104c2819614575fe06
Miguel Rosendo <miguelrosendo29@gmail.com>
2023-10-14 15:09:33 +0100

    base

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Field field = new Field();
+        Character character = new Character(field.getHeight(), field.getWidth());
+    }
+}
