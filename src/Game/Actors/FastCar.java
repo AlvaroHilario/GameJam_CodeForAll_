885cf8fd7e52f4f1f79cedb9a98924c4e92e5a8e
alexandresanchograca@gmail.com <alexandresanchograca@gmail.com>
2023-10-15 12:59:26 +0100

    Fixed Controller and player merge conflit, merged with main

--- src/Game/Actors/FastCar.java
+++ src/Game/Actors/FastCar.java
@@ -1,4 +1,10 @@
 package Game.Actors;
 
+import Game.MovementDir;
+
 public class FastCar extends Car{
+
+    public FastCar(int startCol, int startRow, String carImage, MovementDir moveDir) {
+        super(startCol, startRow, carImage, moveDir);
+    }
 }
