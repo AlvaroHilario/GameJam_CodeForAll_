b81c69a721e0dc25891c28be0f50cb021903dbad
Miguel Rosendo <miguelrosendo29@gmail.com>
2023-10-20 18:18:48 +0100

    removed non iso game

--- src/Game/Grid/LevelDesign.java
+++
@@ -1,5 +0,0 @@
-package Game.Grid;
-
-public class LevelDesign {
-    //Where what is a road and what is a sidewalk will be defined
-}
--- src/Main.java
+++
@@ -1,8 +0,0 @@
-import Game.Cross4All;
-
-public class Main {
-    public static void main(String[] args) {
-        Cross4All gameObj = new Cross4All();
-        gameObj.run();
-    }
-}
